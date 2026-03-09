d02026cc5d05085f1ad1ff7c1e603d32f53a1271
Parikshitgole <parikshitgole@Parikshitgoles-MacBook-Air.local>
2026-03-10 01:08:26 +0530

    newww

--- src/data/mockAuth.js
+++ src/data/mockAuth.js
@@ -2,7 +2,7 @@ import api from "../api/api";
 
 export async function verifyAuth(role, password) {
   try {
-    const response = await api.post("/auth/verify", { role, password });
+    const response = await api.post("/api/auth/verify", { role, password });
     return response.data; // { authorized: true/false }
   } catch (error) {
     console.error("Auth verification failed:", error);
